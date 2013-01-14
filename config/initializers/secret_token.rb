4f874f0731de0aca5bd118129d4888cafcf8d60f
Jure Triglav <juretriglav@gmail.com>
2013-01-14 14:15:35 +0100

    Initial commit with pry, rspec and slim

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tsgb::Application.config.secret_token = '34643f9508832ae6130a533f70f66ee9cd772e839d87c83fc2dcb910f18a87c1077d01abb250999fdfc73250f57be88b2bbe3efda9d454f2b31ff0ef5feea1a3'
