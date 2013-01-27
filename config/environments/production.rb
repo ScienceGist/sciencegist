1fc2e05777dadec96bc93556b088a74024e95834
Jure Triglav <juretriglav@gmail.com>
2013-01-27 17:07:54 +0100

    A differnet way to specify precompiled assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Sg::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile << /(^[^_\/]|\/[^_])[^\/]*$/
+  config.assets.precompile += %w( blog.css.scss base.css.scss )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
