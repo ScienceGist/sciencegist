790ad2f1f97c91a8be1aeb813a0063702b5603ee
Jure Triglav <juretriglav@gmail.com>
2013-07-06 11:59:19 +0100

    Set ruby to 2.0.0

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,7 @@ set :repository,  "git@github.com:jure/tsgb.git"
 set :branch, "master"
 set :use_sudo, false
 set :ssh_options, { :forward_agent => true }
+set :rbenv_ruby_version, "ruby-2.0.0"
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
