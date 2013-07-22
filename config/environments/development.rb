59d6386e265493a3247273b6669841e725fdd4cc
Jure Triglav <juretriglav@gmail.com>
2013-07-22 19:06:51 +0200

    Style fixes for Devise forms. Style fix for alert font weight.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,7 +4,7 @@ Sg::Application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.action_mailer.default_url_options = { :host => 'sciencegist.com' }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   ActionMailer::Base.smtp_settings = {
     :address              => "smtp.gmail.com",
