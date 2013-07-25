9581c091ea545cf82baa80c5d513ae0b6bee317d
Jure Triglav <juretriglav@gmail.com>
2013-07-25 11:49:16 +0200

    Slightly prettier profile view

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -7,5 +7,8 @@ class ProfilesController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @upvoted = @user.find_up_voted_items
+    @downvoted = @user.find_down_voted_items
+    @wrote = @user.gists.order('cached_votes_score desc')
   end
 end
