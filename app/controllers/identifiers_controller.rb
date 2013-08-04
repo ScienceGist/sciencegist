2f2074b87250ad95c6c796ac832d7745d849d0f9
Jure Triglav <juretriglav@gmail.com>
2013-08-04 14:10:37 +0200

    Don't redirect after found identifier, merely render the papers/show partial

--- app/controllers/identifiers_controller.rb
+++ app/controllers/identifiers_controller.rb
@@ -2,34 +2,30 @@ class IdentifiersController < ApplicationController
   def doi
     @doi = "doi: " + params[:identifiers]
     @paper = Paper.find_by_identifier(@doi)
-    if @paper
-      redirect_to paper_path(@paper.id)
-    else
+    unless @paper
       metadata = PaperMetadata.metadata_for(@doi)
       unless metadata[:status] == :NODOI
         @paper = Paper.create!(title: metadata[:title], 
           identifier: @doi, 
           journal: metadata[:journal],
           authors: metadata[:authors])
-        redirect_to paper_path(@paper.id)
       end
     end
+    render 'papers/show' if @paper
   end
 
   def arxiv
     @arxiv = "arXiv: " + params[:identifiers]
     @paper = Paper.find_by_identifier(@arxiv)
-    if @paper
-      redirect_to paper_path(@paper.id)
-    else
+    unless @paper
       metadata = PaperMetadata.metadata_for(@arxiv)
       unless metadata.blank?
         @paper = Paper.create!(title: metadata[:title], 
           identifier: @arxiv, 
           journal: metadata[:journal],
           authors: metadata[:authors])
-        redirect_to paper_path(@paper.id)
       end
     end
+    render 'papers/show' if @paper
   end
 end
